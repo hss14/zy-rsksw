cbdddaf39bbe91a0be0e67ad2b1f7d5de99d01ad
hss14 <aqyz0713hss@163.com>
2016-07-20 10:29:37 +0800

    template.php finish version 1

---
+++ index.php
@@ -0,0 +1,405 @@
+<?php
+	$index = new PageZyrsksw();
+	$index->Display();
+?>
+<!--
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>枣阳人事考试网</title>
+<meta name="description" content="欢迎访问枣阳人事考试网,枣阳市人事考试院,枣阳教育考试网,地址：湖北省枣阳市民主路23号,电话：0710-6311315 " />
+<meta name="keywords" content="枣阳人事考试网,枣阳人事培训考试中心,枣阳教育考试网" />
+<link href="/templets/zaoyang/css/xzrsxxw.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <div class="div900c">
+
+        <div class="head_title marginAuto"></div>
+
+        <div class="head_menu marginAuto">
+
+            <div id="head_A" class="left_10p top_10p" style="float: left;">
+
+                 <a href="/">首页</a>|
+                 
+                     <a   href="/xinwendongtai/" title="新闻动态">新闻动态</a> |
+                
+                     <a   href="/gongshigonggao/" title="公示公告">公示公告</a> |
+                
+                     <a   href="/zhuanyejishukaoshi/" title="专业技术考试">专业技术考试</a> |
+                
+                     <a   href="/shiyedanweikaoshi/" title="事业单位考试">事业单位考试</a> |
+                
+                     <a   href="/shehuihuakaoshi/" title="社会化考试">社会化考试</a> |
+                
+                     <a   href="/gongwuyuankaoshi/" title="公务员考试">公务员考试</a> |
+                
+                     <a   href="/banshizhinan/" title="办事指南">办事指南</a> |
+                
+                     <a   href="/ziliaoxiazai/" title="资料下载">资料下载</a> |
+                
+            </div>
+
+        </div>
+
+
+        <div class="div900c">
+
+            
+
+    <div style="width: 900px; min-height: 400px; height: auto; float: left;">
+
+        <div class="left" style="float: left;">
+      
+            <div class="left_content">
+                <div class="leftImgDiv">
+                    <a href="/fenshu/" target="_blank">
+                        <img src="/templets/zaoyang/images/wscf.png" border="0" width="210"></a>
+                </div>
+            </div>
+            <div class="left_content">
+                <div class="leftImgDiv">
+                    <a href="/zhunkaozheng/" target="_blank">
+                        <img src="/templets/zaoyang/images/wsdyzkz.png" border="0" width="210"></a>
+                </div>
+            </div>
+<br/>
+            <div class="left_content">
+
+                <div class="bottom_titles">
+
+                    <div style="float: left; margin-top: 12px; font-size: 15px; margin-left: 13px; display: inline;
+
+                        color: White;">
+
+                        友情链接</div>
+
+                  
+
+                </div>
+
+                <div class="left_yqlj border_1" style="text-align:left">
+
+                    <ul>     
+ <li><a href='http://zypxks.lxsk.com/' target='_blank'>枣阳市人事培训管理和在线考试系统</a> </li><li><a href='http://www.hbsrsksy.cn/' target='_blank'>湖北人事考试网</a> </li><li><a href='http://www.xyrsks.cn/' target='_blank'>襄阳市人事考试网</a> </li><li><a href='http://www.xzrsksw.com/' target='_blank'>襄州区人事考试网</a> </li><li><a href='http://www.zylb.gov.cn/' target='_blank'>枣阳市人力资源和社会保障局</a> </li><li><a href='http://www.zyjyj.cn/' target='_blank'>枣阳市教育局</a> </li><li><a href='http://www.hbzyfh.com/' target='_blank'>枣阳市第一人民医院</a> </li>
+					</ul>
+
+                  
+
+                </div>
+
+            </div>
+
+            
+
+            
+
+        </div>
+
+        <div style="float: left; width: 10px; height: 100px;">
+
+        </div>
+
+        <div class="middle" style="float: left;">
+
+            <div class="left_top">
+
+                <div class="more" style="text-align: center; width: 60px; float: left;">
+
+                     <a href="/xinwendongtai/">更多</a></div>
+
+                <div class="more new_title">
+
+                    新闻动态</div>
+
+            </div>
+
+            <div class="new_contents">
+				<ul>
+                 <li><a href="/xinwendongtai/2016/0627/1336.html" title="2016年6月27日领证通知" target='_blank'>2016年6月27日领证通知</a><span>16-06-27</span></li>
+<li><a href="/xinwendongtai/2016/0513/1264.html" title="枣阳市人民政府财政投资评审中心2016年度公开招" target='_blank'>枣阳市人民政府财政投资评审中心2016年度公开招</a><span>16-05-13</span></li>
+<li><a href="/xinwendongtai/2016/0425/1263.html" title="关于调整2016年度水平能力测试准考证打印时间的" target='_blank'>关于调整2016年度水平能力测试准考证打印时间的</a><span>16-04-25</span></li>
+<li><a href="/xinwendongtai/2016/0421/1262.html" title="关于教育副高水平能力测试报名的通知" target='_blank'>关于教育副高水平能力测试报名的通知</a><span>16-04-21</span></li>
+<li><a href="/xinwendongtai/2016/0418/1260.html" title="关于2015年度襄阳市机关事业单位工勤人员技术等" target='_blank'>关于2015年度襄阳市机关事业单位工勤人员技术等</a><span>16-04-13</span></li>
+<li><a href="/xinwendongtai/2016/0412/1258.html" title="关于2016年中小学教师中、高级职称专业水平能力" target='_blank'>关于2016年中小学教师中、高级职称专业水平能力</a><span>16-04-12</span></li>
+<li><a href="/xinwendongtai/2016/0406/1256.html" title="关于开展2016年度全省正高职高级工程师(高级农艺" target='_blank'>关于开展2016年度全省正高职高级工程师(高级农艺</a><span>16-04-06</span></li>
+<li><a href="/xinwendongtai/2016/0406/1253.html" title="关于2016年度全省中小学教师申报高级职称水平能" target='_blank'>关于2016年度全省中小学教师申报高级职称水平能</a><span>16-04-06</span></li>
+<li><a href="/xinwendongtai/2016/0401/1251.html" title="关于襄阳市2016年度专业技术人员晋升专业技术职" target='_blank'>关于襄阳市2016年度专业技术人员晋升专业技术职</a><span>16-04-01</span></li>
+<li><a href="/xinwendongtai/2016/0331/1249.html" title="关于做好2016年全省机关事业单位工勤人员技术等" target='_blank'>关于做好2016年全省机关事业单位工勤人员技术等</a><span>16-03-31</span></li>
+
+                  </ul>
+            </div>
+
+            <div style="margin-top: 8px;">
+
+                <div style="width: 312px; min-height: 220px; float: left">
+
+                    <div class="samlltitle">
+
+                        <div class="samll_title more" style="color: White;">
+
+                            图片新闻</div>
+
+                        <div class="more" style="display: inline; text-align: right; float: left; width: 200px;">
+
+                            <a href="/tupianxinwen/">更多...  </a></div>
+
+                    </div>
+
+                    <div class="" style="height: 249px; min-height: 249px;">
+
+
+     <div class="" style="height: 249px; min-height: 249px;">
+
+
+   <div id="slide_x" class="slide_x">
+<div class="box">
+<ul class="list">
+ <li><a  target="_blank" href="/tupianxinwen/2014/1128/808.html" title="“帝乡大舞台·我们的中国梦”文艺汇演人社局专"><img src='/uploads/allimg/141128/5-14112Q50059292-lp.JPG' alt="“帝乡大舞台·我们的中国梦”文艺汇演人社局专" ></a><p>“帝乡大舞台·我们的中国梦”文艺汇演人社局专</p></li>
+
+</ul>
+</div>
+<ul class="btn">
+<li class="b_1 selected">1</li>
+<li class="b_2">2</li>
+<li class="b_3">3</li>
+<li class="b_4">4</li>
+<li class="b_5">5</li>
+</ul>
+</div>
+<script src="/templets/zaoyang/js/jquery-1.4.4.min.js"></script>
+<script src="/templets/zaoyang/js/jquery.cxslide.min.js"></script>
+<script>
+$("#slide_x").cxSlide({plus:true,minus:true});
+</script>
+
+                        
+
+                    </div>
+
+                </div>
+
+                        
+
+                    </div>
+
+                </div>
+
+                <div style="float: left; height: 10px; width: 8px;">
+
+                     </div>
+
+                <div style="width: 312px; min-height: 250px; float: left">
+
+                    <div class="samlltitle">
+
+                        <div class="samll_title more" style="color: White;">
+
+                            公示公告</div>
+
+                        <div class="more" style="display: inline; text-align: right; float: left; width: 200px;">
+
+                            <a href="/gongshigonggao/">更多...  </a></div>
+
+                    </div>
+
+                    <div class="samlltitle_content" style="height: 248px; min-height: 248px;">
+
+                        <ul>
+                            <li><a href="/gongshigonggao/2016/0316/1242.html" title="2015年度全国一级注册消防工程师资格考试成绩查询公告" target='_blank'>2015年度全国一级注册消防工程师资格考试成绩查询公告</a></li>
+<li><a href="/gongshigonggao/2015/1019/1124.html" title="枣阳市2015年公开招聘教师递补公告" target='_blank'>枣阳市2015年公开招聘教师递补公告</a></li>
+<li><a href="/gongshigonggao/2015/0826/1113.html" title="枣阳市2015年公开招聘教师拟录用人员名单公示" target='_blank'>枣阳市2015年公开招聘教师拟录用人员名单公示</a></li>
+<li><a href="/gongshigonggao/2015/0818/1108.html" title="枣阳市2015年公开招聘乡镇小学教师成绩公示" target='_blank'>枣阳市2015年公开招聘乡镇小学教师成绩公示</a></li>
+<li><a href="/gongshigonggao/2015/0813/1107.html" title="枣阳市2015年公开招聘高中教师综合成绩公示" target='_blank'>枣阳市2015年公开招聘高中教师综合成绩公示</a></li>
+<li><a href="/gongshigonggao/2015/0813/1106.html" title="枣阳市2015年公开招聘幼师体检、政审通知" target='_blank'>枣阳市2015年公开招聘幼师体检、政审通知</a></li>
+<li><a href="/gongshigonggao/2015/0813/1105.html" title="枣阳市2015年公开招聘乡镇小学教师面试公告" target='_blank'>枣阳市2015年公开招聘乡镇小学教师面试公告</a></li>
+<li><a href="/gongshigonggao/2015/0811/1104.html" title="枣阳市2015年公开招聘高中教师面试公告" target='_blank'>枣阳市2015年公开招聘高中教师面试公告</a></li>
+<li><a href="/gongshigonggao/2015/0810/1103.html" title="枣阳市2015年公开招聘幼儿教师综合成绩公示" target='_blank'>枣阳市2015年公开招聘幼儿教师综合成绩公示</a></li>
+
+
+                      </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    
+
+    <div style="float: left; margin-top: 5px;">
+
+        <div class="bottom_e">
+
+            <div class="bottom_title">
+
+                <div class="samll_left_title more" style="color: White;">
+
+                    专业技术考试
+
+                </div>
+
+                <div class="more" style="display: inline; text-align: right; float: left; width: 320px;">
+
+                    <a href="/zhuanyejishukaoshi/">更多...</a>
+
+                </div>
+
+            </div>
+
+            <div class="bottom_c border_1">
+
+              <ul>
+                  <li><a href="/zhuanyejishukaoshi/2016/0412/1259.html" title="关于2016年中小学教师中、高级职称专业水平能力测试的通知" target='_blank'>关于2016年中小学教师中、高级职称专业水平能力测试的通知</a></li>
+<li><a href="/zhuanyejishukaoshi/2016/0406/1257.html" title="关于开展2016年度全省正高职高级工程师(高级农艺师)水平能力测试工作的通知" target='_blank'>关于开展2016年度全省正高职高级工程师(高级农艺师)水平能力测试工作的通知</a></li>
+<li><a href="/zhuanyejishukaoshi/2016/0406/1254.html" title="关于2016年度全省中小学教师申报高级职称水平能力测试工作的通知" target='_blank'>关于2016年度全省中小学教师申报高级职称水平能力测试工作的通知</a></li>
+<li><a href="/zhuanyejishukaoshi/2016/0401/1252.html" title="关于襄阳市2016年度专业技术人员晋升专业技术职务水平能力测试工作方案的通知" target='_blank'>关于襄阳市2016年度专业技术人员晋升专业技术职务水平能力测试工作方案的通知</a></li>
+<li><a href="/zhuanyejishukaoshi/2015/1203/1209.html" title="关于2016年度全国专业技术人员职称外语等级统一考试工作的通知" target='_blank'>关于2016年度全国专业技术人员职称外语等级统一考试工作的通知</a></li>
+<li><a href="/zhuanyejishukaoshi/2015/1201/1208.html" title="关于打印准考证（2015年12月初级、中级计算机考试）的通知" target='_blank'>关于打印准考证（2015年12月初级、中级计算机考试）的通知</a></li>
+<li><a href="/zhuanyejishukaoshi/2015/0825/1111.html" title="2015年教育系统中、高级和卫生系统高级水平能力测试合格证书领取通知" target='_blank'>2015年教育系统中、高级和卫生系统高级水平能力测试合格证书领取通知</a></li>
+
+               </ul>
+            </div>
+
+        </div>
+
+        <div class="space_bottom">
+
+        </div>
+
+        <div class="bottom_e">
+
+            <div class="bottom_title">
+
+                <div class="samll_left_title more" style="color: White;">
+
+                    事业单位考试
+
+                </div>
+
+                <div class="more" style="display: inline; text-align: right; float: left; width: 320px;">
+
+                    <a href="/shiyedanweikaoshi/">更多...</a>
+
+                </div>
+
+            </div>
+
+            <div class="bottom_c border_1">
+               <ul>
+                    <li><a href="/shiyedanweikaoshi/2016/0418/1261.html" title="关于2015年度襄阳市机关事业单位工勤人员技术等级考核结果的通知" target='_blank'>关于2015年度襄阳市机关事业单位工勤人员技术等级考核结果的通知</a></li>
+<li><a href="/shiyedanweikaoshi/2016/0331/1250.html" title="关于做好2016年全省机关事业单位工勤人员技术等级考核工作的通知" target='_blank'>关于做好2016年全省机关事业单位工勤人员技术等级考核工作的通知</a></li>
+<li><a href="/shiyedanweikaoshi/2015/1019/1128.html" title="关于领取2014年工人技术等级考核证书的通知" target='_blank'>关于领取2014年工人技术等级考核证书的通知</a></li>
+<li><a href="/shiyedanweikaoshi/2015/0928/1119.html" title="关于做好2015年湖北省机关事业单位高级技师评审材料报送工作的通知" target='_blank'>关于做好2015年湖北省机关事业单位高级技师评审材料报送工作的通知</a></li>
+<li><a href="/shiyedanweikaoshi/2015/0424/864.html" title="关于做好2015年全市机关事业单位工勤人员技术等级考核工作的通知" target='_blank'>关于做好2015年全市机关事业单位工勤人员技术等级考核工作的通知</a></li>
+<li><a href="/shiyedanweikaoshi/2014/1103/451.html" title="2014年全市机关事业单位工勤人员技术等级工培训与技能考核安排" target='_blank'>2014年全市机关事业单位工勤人员技术等级工培训与技能考核安排</a></li>
+<li><a href="/shiyedanweikaoshi/2014/1103/450.html" title="2014年工考全市高级技师、技师和市区单位等级工培训与技能考核安排" target='_blank'>2014年工考全市高级技师、技师和市区单位等级工培训与技能考核安排</a></li>
+
+                 </ul>
+            </div>
+
+        </div>
+
+        <div class="bottom_e">
+
+            <div class="bottom_title">
+
+                <div class="samll_left_title more" style="color: White;">
+
+                    社会化考试
+
+                </div>
+
+                <div class="more" style="display: inline; text-align: right; float: left; width: 320px;">
+
+                    <a href="/shehuihuakaoshi/">更多...</a>
+
+                </div>
+
+            </div>
+
+            <div class="bottom_c border_1">
+
+              <ul>
+                    <li><a href="/shehuihuakaoshi/2016/0513/1265.html" title="枣阳市人民政府财政投资评审中心2016年度公开招聘专业技术人员简章" target='_blank'>枣阳市人民政府财政投资评审中心2016年度公开招聘专业技术人员简章</a></li>
+<li><a href="/shehuihuakaoshi/2016/0316/1240.html" title="关于2016年度环境影响评价工程师职业资格考试工作的通知" target='_blank'>关于2016年度环境影响评价工程师职业资格考试工作的通知</a></li>
+<li><a href="/shehuihuakaoshi/2016/0315/1238.html" title="关于2016年度社会工作者职业水平考试工作的通知" target='_blank'>关于2016年度社会工作者职业水平考试工作的通知</a></li>
+<li><a href="/shehuihuakaoshi/2016/0308/1236.html" title="关于2016年度注册计量师资格考试工作的通知" target='_blank'>关于2016年度注册计量师资格考试工作的通知</a></li>
+<li><a href="/shehuihuakaoshi/2016/0308/1234.html" title="关于2016年度翻译专业资格(水平)笔译考试工作的通知" target='_blank'>关于2016年度翻译专业资格(水平)笔译考试工作的通知</a></li>
+<li><a href="/shehuihuakaoshi/2016/0223/1231.html" title="关于2016年度全国监理工程师资格考试工作的通知" target='_blank'>关于2016年度全国监理工程师资格考试工作的通知</a></li>
+<li><a href="/shehuihuakaoshi/2016/0223/1229.html" title="关于2016年度投资建设项目管理师职业水平考试工作的通知" target='_blank'>关于2016年度投资建设项目管理师职业水平考试工作的通知</a></li>
+
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="space_bottom">
+
+        </div>
+
+        <div class="bottom_e">
+
+            <div class="bottom_title">
+
+                <div class="samll_left_title more" style="color: White;">
+
+                    公务员考试
+
+                </div>
+
+                <div class="more" style="display: inline; text-align: right; float: left; width: 320px;">
+
+                    <a href="/gongwuyuankaoshi/">更多...</a>
+
+                </div>
+
+            </div>
+
+            <div class="bottom_c border_1">
+                <ul>
+					 <li><a href="/gongwuyuankaoshi/2015/1222/1214.html" title="市人事考试院全面启动2015年度国家公务员培训和专业技术人员公需科目" target='_blank'>市人事考试院全面启动2015年度国家公务员培训和专业技术人员公需科目</a></li>
+<li><a href="/gongwuyuankaoshi/2015/0928/1121.html" title="2015年湖北省省直机关（单位）公开遴选公务员公告" target='_blank'>2015年湖北省省直机关（单位）公开遴选公务员公告</a></li>
+<li><a href="/gongwuyuankaoshi/2014/1128/806.html" title="关于在全市公务员和专业技术人员中开展社会主义核心价值观培训学习" target='_blank'>关于在全市公务员和专业技术人员中开展社会主义核心价值观培训学习</a></li>
+
+                 </ul>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+      
+
+                <div class="footer footerimg marginAuto" style="line-height:140%;font-size:13px;">
+
+            <A title="工信部备案许可证" href="http://www.miibeian.gov.cn/" target="_blank" rel="nofollow">鄂ICP备14015568号-1</a>　
+
+技术支持：<A title="襄阳华维网络" href="http://www.huavee.com/" target="_blank" rel="nofollow">襄阳华维网络</a>
+
+            <BR>
+版权所有<font color="#4e5766">：枣阳市人事考试院</font>
+            <BR>地址：湖北省枣阳市民主路23号　　　 电话：0710-6311315 
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253584961'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/stat.php%3Fid%3D1253584961%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+
+
+        </div>
+
+    </div>
+
+    </form>
+
+</body>
+
+</html>
+-->
