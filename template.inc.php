b8ddf9488242328e492f3339ef6b282ff530a0a8
hss14 <aqyz0713hss@163.com>
2016-07-20 11:27:34 +0800

    modified

--- template.inc.php
+++ template.inc.php
@@ -25,9 +25,14 @@
 		}
 
 		public function DisplayHead( ) {
-			echo "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>\n";
-			echo "<html xmlns='http://www.w3.org/1999/xhtml'>\n";
-			echo "<head><br/>";
+?>
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
 			echo "\t<title>".$this->title."</title>\n";
 			echo "\t<link href='".$this->style."' rel='stylesheet' type='text/css' />\n";
 ?>
